28ae853d3227677d81e2a05b5bd59ea5cf7ab46a
agutnichenko <quo_vadis@i.ua>
2016-11-15 12:57:10 +0200

    Upcase to downcase

--- db/migrate/20161114143357_create_cards.rb
+++ db/migrate/20161114143357_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.string :Original_text
-      t.string :Translated_text
-      t.float :Review_date
+      t.string :original_text
+      t.string :translated_text
+      t.float :review_date
       t.timestamps
     end
   end
