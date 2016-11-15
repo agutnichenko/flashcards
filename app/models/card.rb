b7efce2acde7ed8a3665213792d9c9424dcbc7e9
agutnichenko <quo_vadis@i.ua>
2016-11-15 16:07:37 +0200

    added constraints on Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,5 @@
 class Card < ApplicationRecord
 
-  # validates :translated_text, {numericality: {allow_nil: false} }
-  # validates :review_date, {numericality: {allow_nil: false} }
+  validates :original_text, :translated_text, presence: true
 
 end
