b8c8e8ffef2f73910d9232d9991b643fde3c8342
agutnichenko <quo_vadis@i.ua>
2016-11-17 11:43:08 +0200

    changed migration

--- db/migrate/20161117093836_create_cards.rb
+++ db/migrate/20161117093836_create_cards.rb
@@ -1,8 +1,8 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.string :original_text, nil: false
-      t.string :translated_text, nil: false
+      t.string :original_text, null: false
+      t.string :translated_text, null: false
       t.date :review_date
       t.timestamps
     end
