cfe452359d2605188b94c8955f55c35cf24154b4
agutnichenko <quo_vadis@i.ua>
2016-11-21 18:23:11 +0200

    added delete method

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,4 +4,47 @@ class CardsController < ApplicationController;
     @cards = Card.all
   end
 
+  def show
+    unless @card = Card.find(params[:id])
+    #unless @card = Card.where(id: params[:id]).first
+      render text: 'Page not found', status: 404
+    end
+  end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+    params.permit!
+    @card = Card.find(params[:id])
+  end
+
+  def create
+    params.permit!
+    @card = Card.create(params[:card])
+    if @card.errors.empty?
+      redirect_to card_path(@card), notice: 'card is created!'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    params.permit!
+    @card = Card.find(params[:id])
+    @card.update_attributes(params[:card])
+    if @card.errors.empty?
+      redirect_to card_path(@card)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+    @card.destroy
+    redirect_to action: "index"
+  end
+
 end
