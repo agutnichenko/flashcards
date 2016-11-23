1e54cbc2be88939f27f354961262265a80df3b4e
agutnichenko <quo_vadis@i.ua>
2016-11-23 18:20:24 +0200

    added partial and is_automatically_set

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,10 @@ class Card < ApplicationRecord
   validates :original_text, presence: true
   validates :translated_text, presence: true
   #validates :review_date, presence: true
+  before_create :is_automatically_set
+
+  def is_automatically_set
+    self.review_date = Time.new.to_s[0..-18] + (Time.new.day + 3).to_s
+  end
+
 end
