011c04b70b6dc45f45fc10d5782981174b2e8f0c
agutnichenko <quo_vadis@i.ua>
2016-11-24 16:06:17 +0200

    helper

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,7 @@
 class Card < ApplicationRecord
   validates :original_text, presence: true
   validates :translated_text, presence: true
-  #validates :review_date, presence: true
+  #railsssvalidates :review_date, presence: true
   before_create :is_automatically_set
 
   def is_automatically_set
