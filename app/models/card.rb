b2bd27daa860826f53c337f9e6c9c78246d80a51
agutnichenko <quo_vadis@i.ua>
2016-11-28 16:40:45 +0200

    dded check_uniqueness method

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,10 +2,17 @@ class Card < ApplicationRecord
   validates :original_text, presence: true
   validates :translated_text, presence: true
   validates :review_date, presence: true
+  validate :check_uniqueness
   before_validation :is_automatically_set
 
   def is_automatically_set
     self.review_date = 3.days.from_now
+  end
+
+  def check_uniqueness
+    unless self.original_text.downcase != self.translated_text.downcase
+      errors.add(:base, 'translated text should not be equal to original one')
+    end
   end
 
 end
