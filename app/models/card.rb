254b4e346c51d0807cde1f0a75f701ea587599e1
agutnichenko <quo_vadis@i.ua>
2016-11-28 16:10:23 +0200

    changed before_create to before_validation

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,8 +1,8 @@
 class Card < ApplicationRecord
   validates :original_text, presence: true
   validates :translated_text, presence: true
-  #railsssvalidates :review_date, presence: true
-  before_create :is_automatically_set
+  validates :review_date, presence: true
+  before_validation :is_automatically_set
 
   def is_automatically_set
     self.review_date = 3.days.from_now
