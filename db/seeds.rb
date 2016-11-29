07a69fcbe00b5d818f8dc8b2bc1765aa272756a2
agutnichenko <quo_vadis@i.ua>
2016-11-29 15:10:36 +0200

    tried to populate db with nokogiri xpath output via seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'nokogiri'
+require 'open-uri'
+
+page = Nokogiri::HTML(open('http://www.languagedaily.com/learn-german/vocabulary/common-german-words'))
+
+page.xpath("//td[2]").each do |or_words|
+  p or_words.text
+  Card.create(original_text: or_words.text)
+end
+
+page.xpath("//td[3]").each do |tr_words|
+  Card.create(translated_text: tr_words.text)
+end
