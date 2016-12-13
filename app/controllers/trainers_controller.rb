1488a115ef16fba1306d827583afa6f4d1735fb6
agutnichenko <quo_vadis@i.ua>
2016-12-13 21:36:17 +0200

    form, route

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -3,11 +3,13 @@ class TrainersController < ApplicationController
   before_filter :find_card, only: [:review]
 
   def review
-    params.permit!
     if @card_translation.translated_text == params[:translated_text]
       redirect_to root_url, notice: 'перевод правильный'
+      #p gets.chomp
+      p @card_translation.translated_text
     else
-      redirect_to root_url, notice: 'перевод неправильный'
+      redirect_to cards_url, notice: 'перевод неправильный'
+      #p gets.chomp
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-
-  #get 'trainers/index'
-
-  # get 'new' => 'cards#new'
-  # get 'delete' => 'cards#destroy'
-  # get 'edit' => 'cards#edit'
-  # get 'cards' => 'cards#index'
-  # get 'home' => 'home#index'
   root 'home#index'
-  get 'trainers/:id/review', to: 'trainers#review', as: 'trainer'
+  post 'trainers/:id/review', to: 'trainers#review', as: 'trainer'
   resources :cards
   resources :home
 end
