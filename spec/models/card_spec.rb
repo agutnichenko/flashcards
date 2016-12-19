351f864b49b3acc0b2868ce3c9bb1734a9728796
agutnichenko <quo_vadis@i.ua>
2016-12-19 19:10:06 +0200

    two tests

---
+++ spec/models/card_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Card, :type => :model do
+  it "check uniqueness" do
+    card = Card.new(original_text: "ds", translated_text: "ds")
+    expect(card.check_uniqueness).to include "translated text should not be equal to original one"
+  end
+end
+
