c2852fa6e251fd7ec2e25191c34f654bad9ce782
agutnichenko <quo_vadis@i.ua>
2016-12-23 18:24:03 +0200

    label

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe CardsController, :type => :feature do
       visit new_card_path
       # fill_in 'card_original_text', with: 'qwerty'
       # fill_in 'card_translated_text', with: 'qwerty'
-      fill_in 'original text', with: 'qwerty'
-      fill_in 'translated text', with: 'qwerty'
+      fill_in 'Word one', with: 'qwerty'
+      fill_in 'Word two', with: 'qwerty'
       click_button 'Create flashcard'
       expect(page).to have_current_path(cards_path)
     end
