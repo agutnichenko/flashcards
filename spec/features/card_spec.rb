2c2e0f1cc934ae6c5d7c06802400fd8b61de2340
agutnichenko <quo_vadis@i.ua>
2016-12-23 16:01:22 +0200

    added travis yml

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe CardsController, :type => :feature do
 
     it "check_uniqueness" do
       visit new_card_path
-      fill_in 'original_text', with: 'qwerty'
+      fill_in 'original text', with: 'qwerty'
       fill_in 'translated_text', with: 'qwerty'
       click_button 'Sign in'
       expect(response).to render_template("new")
