d1af3902a13196bc30bdeea8c6a6d9aaafbfa86d
agutnichenko <quo_vadis@i.ua>
2016-12-23 16:33:04 +0200

    label

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -5,10 +5,12 @@ RSpec.describe CardsController, :type => :feature do
 
     it "check_uniqueness" do
       visit new_card_path
+      # fill_in 'card_original_text', with: 'qwerty'
+      # fill_in 'card_translated_text', with: 'qwerty'
       fill_in 'original text', with: 'qwerty'
-      fill_in 'translated_text', with: 'qwerty'
-      click_button 'Sign in'
-      expect(response).to render_template("new")
+      fill_in 'translated text', with: 'qwerty'
+      click_button 'Create flashcard'
+      expect(page).to have_current_path(cards_path)
     end
 
   end
