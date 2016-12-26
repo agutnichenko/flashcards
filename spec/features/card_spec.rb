16d2082caa5894c28b5791fbef29360c3219fa90
agutnichenko <quo_vadis@i.ua>
2016-12-26 11:58:16 +0200

    removed tests from feature

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -11,15 +11,5 @@ RSpec.describe CardsController, :type => :feature do
       expect(page).to have_current_path(cards_path)
     end
 
-    it "check_date method" do
-      card = FactoryGirl.create(:card)
-      expect(card.review_date).to eq(3.days.from_now)
-    end
-
-    it "check random method" do
-      card = FactoryGirl.create(:card)
-      expect(card.id).not_to be NIL
-    end
-
   end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe Card, :type => :model do
     expect(card.check_uniqueness).to include "translated text should not be equal to original one"
   end
 
+  it "check_date method" do
+    card = FactoryGirl.create(:card)
+    expect(card.review_date).to eq(3.days.from_now)
+  end
+
+  it "check random method" do
+    card = FactoryGirl.create(:card)
+    expect(card.id).not_to be NIL
+  end
+
 end
 
