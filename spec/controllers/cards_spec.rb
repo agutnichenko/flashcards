72f30cbebe08e5a2bf91b5d65a573f9d3f557874
agutnichenko <quo_vadis@i.ua>
2017-01-03 14:01:02 +0200

    fixed erros according to houndci

--- spec/controllers/cards_spec.rb
+++ spec/controllers/cards_spec.rb
@@ -1,70 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe CardsController, :type => :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
 
-    it "responds successfully with an HTTP 200 status code" do
+    it 'responds successfully with an HTTP 200 status code' do
       get :index
       expect(response).to have_http_status(200)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get :index
-      expect(response).to render_template("index")
+      expect(response).to render_template('index')
     end
   end
 
-  describe "GET #show" do
+  describe 'GET #show' do
 
-    it "renders the show template if item found" do
+    it 'renders the show template if item found' do
       card = FactoryGirl.create(:card)
       get :show, params: {id: card.id}
-      expect(response).to render_template("show")
+      expect(response).to render_template('show')
     end
   end
 
-  describe "POST #create" do
+  describe 'POST #create' do
 
-    it "redirects to newly created item" do
+    it 'redirects to newly created item' do
       post :create, params: {card: {original_text: 'as', translated_text: 'qw'}}
       expect(response).to redirect_to(card_path(assigns(:card)))
     end
 
-    it "renders new view in case if translated_text field is empty" do
+    it 'renders new view in case if translated_text field is empty' do
       post :create, params: {card: {original_text: 'as', translated_text: nil}}
-      expect(response).to render_template("new")
+      expect(response).to render_template('new')
     end
 
-    it "renders new view in case if original_text field is empty" do
+    it 'renders new view in case if original_text field is empty' do
       post :create, params: {card: {translated_text: 'asy', original_text: nil}}
-      expect(response).to render_template("new")
+      expect(response).to render_template('new')
     end
-
-    # it "new record?" do
-    #   card = Card.new(original_text: "f", translated_text: "ew")
-    #   expect(response).to render_template("new")
-    # end
-
   end
 
-  describe "PUT #update" do
+  describe 'PUT #update' do
 
-    it "redirects to newly updated item" do
+    it 'redirects to newly updated item' do
       card = FactoryGirl.create(:card)
       put :update, params: {id: card.id, card: {original_text: 'as'}}
       expect(response).to redirect_to(card_path(assigns(:card)))
     end
 
-    it "renders update view in case if update failed" do
+    it 'renders update view in case if update failed' do
       card = FactoryGirl.create(:card)
       put :update, params: {id: card.id, card: {original_text: nil}}
-      expect(response).to render_template("edit")
+      expect(response).to render_template('edit')
     end
   end
 
-  describe "DELETE #delete" do
+  describe 'DELETE #delete' do
 
-    it "renders all cards url in case if card is destroyed" do
+    it 'renders all cards url in case if card is destroyed' do
       card = FactoryGirl.create(:card)
       delete :destroy, params: {id: card.id}
       expect(response).to redirect_to(cards_url)
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,17 +1,17 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Card, :type => :model do
-  it "check uniqueness" do
-    card = Card.new(original_text: "ds", translated_text: "ds")
-    expect(card.check_uniqueness).to include "translated text should not be equal to original one"
+  it 'check uniqueness' do
+    card = Card.new(original_text: 'ds', translated_text: 'ds')
+    expect(card.check_uniqueness).to include 'translated text should not be equal to original one'
   end
 
-  it "check_date method" do
+  it 'check_date method' do
     card = FactoryGirl.create(:card)
     expect(card.review_date.strftime('%a, %e %b %Y')).to eq(3.days.from_now.strftime('%a, %e %b %Y'))
   end
 
-  it "check random method" do
+  it 'check random method' do
     card = FactoryGirl.create(:card)
     expect(card.id).not_to be_nil
   end
@@ -23,19 +23,18 @@ RSpec.describe Card, :type => :model do
     expect(card.errors[:original_text]).to include 'has already been taken'
   end
 
-  it "original_text presence" do
+  it 'original_text presence' do
     card = Card.new(original_text: '', translated_text: 'asdfg')
     expect(card.errors[:original_text]).not_to be_nil
   end
 
-  it "translated_text presence" do
+  it 'translated_text presence' do
     card = Card.new(original_text: 'blablabvcnc', translated_text: '')
     expect(card.errors[:translated_text]).not_to be_nil
   end
 
-  it "translated_text presence" do
+  it 'translated_text presence' do
     card = Card.new(original_text: 'blablabvcnc', translated_text: '')
     expect(card.errors[:translated_text]).not_to be_nil
   end
-
 end
