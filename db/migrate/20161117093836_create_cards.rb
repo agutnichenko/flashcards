03d6c8bf7b0bdcda81bc40847c17326f1d183033
agutnichenko <quo_vadis@i.ua>
2017-01-07 16:49:48 +0200

    added association

--- db/migrate/20161117093836_create_cards.rb
+++ db/migrate/20161117093836_create_cards.rb
@@ -4,6 +4,7 @@ class CreateCards < ActiveRecord::Migration[5.0]
       t.string :original_text, null: false
       t.string :translated_text, null: false
       t.date :review_date
+      t.integer :user_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161117093836) do
+ActiveRecord::Schema.define(version: 20170107123003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,18 @@ ActiveRecord::Schema.define(version: 20161117093836) do
     t.date     "review_date"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "login"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users_cards", force: :cascade do |t|
+    t.integer "card_id"
+    t.integer "user_id"
   end
 
 end
