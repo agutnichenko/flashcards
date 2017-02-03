a488b103b01cc91f253b86486ae17d01a12c1454
agutnichenko <quo_vadis@i.ua>
2017-02-03 12:42:31 +0200

    small fix

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,4 @@
-require 'capybara/rspec'
-require 'factory_girl_rails'
-require 'sorcery'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
