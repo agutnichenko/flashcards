c529e33855893fc43ec7b2953ad0aa4cffcd1a6f
agutnichenko <quo_vadis@i.ua>
2017-02-16 09:14:27 +0200

    edited users_path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, as: :user, url: user_path, method: :patch, html: { class: 'edit_user'}, notice: 'User was successfully updated.' }
+        format.html { redirect_to users_path, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
