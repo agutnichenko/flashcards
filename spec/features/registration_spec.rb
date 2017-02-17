42291ed50c43cb0a9bb0cc1618a1e538ca0f99a4
agutnichenko <quo_vadis@i.ua>
2017-02-17 16:05:27 +0200

    added registration features

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -2,9 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationsController, :type => :feature do
-  describe 'session controller methods' do
+  describe 'registration controller methods' do
 
-    it 'check that user can login with valid credentials' do
+    it 'check that registartion with valid data' do
       visit root_path
       click_link 'Register'
       fill_in 'Email', with: 'blwvhwfdfdfdhmhmhntwkejbv@i.ua'
@@ -12,6 +12,16 @@ RSpec.describe RegistrationsController, :type => :feature do
       fill_in 'Password confirmation', with: '123456'
       click_button 'Create User'
       expect(page).to have_content 'Cards'
+    end
+
+    it 'check that registartion with mismatching passwords fails' do
+      visit root_path
+      click_link 'Register'
+      fill_in 'Email', with: 'uiykjrtgwgwrgwrg'
+      fill_in 'Password', with: '123456'
+      fill_in 'Password confirmation', with: '1234567'
+      click_button 'Create User'
+      expect(page).to have_content 'Password confirmation doesn\'t match Password'
     end
 
   end
