0eb0b6965d2fd40e023954149813e2293dfe13a8
agutnichenko <quo_vadis@i.ua>
2017-03-02 21:27:29 +0200

    added feature test

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -17,5 +17,15 @@ RSpec.describe CardsController, :type => :feature do
       expect(page).to have_current_path(cards_path)
     end
 
+    it 'check_uniqueness' do
+      visit new_registration_path
+      fill_in 'Email', with: 'blwvhwfdfdfdhmhmhntwkejbv@i.ua'
+      fill_in 'Password', with: '123456'
+      fill_in 'Password confirmation', with: '123456'
+      click_button 'Create User'
+      click_link 'Add card'
+      expect(page).to have_xpath '//*[@id="card_avatar"]'
+    end
+
   end
 end
