ef882e16108b50457de138dc2e988ef36c0c3afb
agutnichenko <quo_vadis@i.ua>
2017-03-04 21:34:04 +0200

    added migration

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,7 +2,7 @@ class TrainersController < ApplicationController
   skip_before_action :require_login
 
   def review
-    @card_translation = Card.find(params[:id])
+    @card_translation = current_user.cards.find(params[:id])
     if @card_translation.original_text == params[:original_text]
       @card_translation.review_date = 3.days.from_now
       @card_translation.save
