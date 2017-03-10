042a3ec59628f2f05de2ec9bb5b5fddad83eaf79
agutnichenko <quo_vadis@i.ua>
2017-03-10 20:23:44 +0200

    added interactor

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,14 +1,9 @@
 class TrainersController < ApplicationController
   skip_before_action :require_login
 
-  def review
-    if @card_translation.original_text == params[:original_text]
-      @card_translation.review_date = 3.days.from_now
-      @card_translation.save
-      redirect_to root_url, notice: 'перевод правильный'
-    else
-      redirect_to root_url, notice: 'перевод неправильный'
+    def review
+      result = CheckCard.call(params: card_params, user: current_user)
+      redirect_to :back, notice: result.notice
     end
-  end
 
 end
--- app/interactors/find_card.rb
+++
@@ -1,7 +0,0 @@
-class FindCard
-  include Interactor
-
-  def find
-    @card_translation = current_user.cards.find(params[:id])
-  end
-end
