31d493537bb7d82c389b935fe48c547e885c0111
agutnichenko <quo_vadis@i.ua>
2017-03-11 14:46:34 +0200

    fixed interactor

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,8 +2,16 @@ class TrainersController < ApplicationController
   skip_before_action :require_login
 
     def review
-      result = CheckCard.call(params: Card.card_params, user: current_user)
-      redirect_to :back, notice: result.notice
+      result = CheckCard.call(params: card_params, user: current_user)
+      if result.success?
+        redirect_to root_url, notice: 'перевод правильный'
+      else
+        redirect_to root_url, notice: 'перевод неправильный'
+      end
     end
+
+  def card_params
+    params.require(:card).permit(:card_params)
+  end
 
 end
--- app/interactors/check_card.rb
+++ app/interactors/check_card.rb
@@ -1,14 +1,16 @@
 class CheckCard
   include Interactor
 
-  def index
-    @card_translation = context.user.cards.find(params[:id])
-    if @card_translation.original_text == context.params[:original_text]
+  def self.call(args = {})
+    context = args
+     @card_translation = context(user.cards.find(card_params[:id]))
+     if @card_translation.original_text == context(card_params[:original_text])
       @card_translation.review_date = 3.days.from_now
-      @card_translation.save
-      redirect_to root_url, notice: 'перевод правильный'
+      result = @card_translation.save
+      context.result = result
     else
-      redirect_to root_url, notice: 'перевод неправильный'
+      context.fail
     end
   end
+
 end
