a36fb059d84165e1c1967ec5d00f32dfb3f10623
agutnichenko <quo_vadis@i.ua>
2017-03-11 11:35:04 +0200

    added Card class

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -2,7 +2,7 @@ class TrainersController < ApplicationController
   skip_before_action :require_login
 
     def review
-      result = CheckCard.call(params: card_params, user: current_user)
+      result = CheckCard.call(params: Card.card_params, user: current_user)
       redirect_to :back, notice: result.notice
     end
 
