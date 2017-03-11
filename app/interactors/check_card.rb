9933db1231e4849471e8320a665fbfc6c7cbf338
agutnichenko <quo_vadis@i.ua>
2017-03-11 11:16:28 +0200

    fixed interactor

--- app/interactors/check_card.rb
+++ app/interactors/check_card.rb
@@ -2,8 +2,8 @@ class CheckCard
   include Interactor
 
   def index
-    @card_translation = current_user.cards.find(params[:id])
-    if @card_translation.original_text == params[:original_text]
+    @card_translation = context.user.cards.find(params[:id])
+    if @card_translation.original_text == context.params[:original_text]
       @card_translation.review_date = 3.days.from_now
       @card_translation.save
       redirect_to root_url, notice: 'перевод правильный'
