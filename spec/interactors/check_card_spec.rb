b2e9079e893f9bb5d16b52276d74399759462327
agutnichenko <quo_vadis@i.ua>
2017-03-18 20:25:26 +0200

    added missing spaces and deleted two migrations

--- spec/interactors/check_card_spec.rb
+++ spec/interactors/check_card_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after first successful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 0, counter_incorrect: 0)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(12.hours.from_now.strftime('%a, %e %b %Y'))
   end
@@ -12,7 +12,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after second successful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 1, counter_incorrect: 0)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(3.days.from_now.strftime('%a, %e %b %Y'))
   end
@@ -20,7 +20,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after third successful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 2, counter_incorrect: 0)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(7.days.from_now.strftime('%a, %e %b %Y'))
   end
@@ -28,7 +28,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after fourth successful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 3, counter_incorrect: 0)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(14.days.from_now.strftime('%a, %e %b %Y'))
   end
@@ -36,7 +36,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after fifth successful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 4, counter_incorrect: 0)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(1.month.from_now.strftime('%a, %e %b %Y'))
   end
@@ -44,7 +44,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after first unsuccessful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 0, counter_incorrect: 1)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(12.hours.from_now.strftime('%a, %e %b %Y'))
   end
@@ -52,7 +52,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after first unsuccessful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 0, counter_incorrect: 2)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(result.review_date.strftime('%a, %e %b %Y')).to eq(12.hours.from_now.strftime('%a, %e %b %Y'))
   end
@@ -60,7 +60,7 @@ RSpec.describe Card, :type => :iteractor do
   it 'check review_date after third unsuccessful attempt ' do
     @current_user = FactoryGirl.create(:user)
     @card = FactoryGirl.create(:card, user: @current_user, counter_correct: 0, counter_incorrect: 3)
-    @params = {original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect}
+    @params = { original_text: @card.original_text, id: @card.id, counter_correct: @card.counter_correct, counter_incorrect: @card.counter_incorrect }
     result = CheckCard.call(user: @current_user, params: @params)
     expect(@card.review_date.strftime('%a, %e %b %Y')).to eq(Time.now.strftime('%a, %e %b %Y'))
   end
