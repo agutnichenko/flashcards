366d7de8806e96e35228ddce2290d951a644eb29
agutnichenko <quo_vadis@i.ua>
2017-03-26 16:50:20 +0300

    declared user variable

--- app/mailers/cards_mailer.rb
+++ app/mailers/cards_mailer.rb
@@ -2,9 +2,11 @@ class CardsMailer < ApplicationMailer
   default from: 'from@example.com'
   layout 'mailer'
 
-  def pending_cards_notification(email)
+  def pending_cards_notification(user)
+    @user = user
+    @email = user.email
     @url  = 'http://example.com/login'
-    mail(to: email, subject: 'you have cards ready for translation')
+    mail(to: @email, subject: 'you have cards ready for translation')
   end
 
 end
