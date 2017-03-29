1e85f0a3efac8ad9d6cec1635a7ca869e5fa1580
agutnichenko <quo_vadis@i.ua>
2017-03-29 22:26:33 +0300

    added locale for registered user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = if logged_in?
+                    current_user.language
+                  elsif params[:locale]
+                    params[:locale]
+                  else
+                    extract_locale_from_accept_language_header
+                  end
+  end
 
   private
+
+  def extract_locale_from_accept_language_header
+    params[:locale] || request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first.presence
+  end
+
   def not_authenticated
     redirect_to login_path, alert: 'Please login first'
   end
