ea7197dd84b6988ee3c3ed98aef1cf828d1dbc82
agutnichenko <quo_vadis@i.ua>
2017-04-30 22:00:43 +0300

    home/dashboard added

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,31 @@
 Rails.application.routes.draw do
-  get 'oauths/oauth'
-  get 'oauths/callback'
-  resource :user, only: [:edit, :update, :show]
+
   root 'home#index'
-  post 'trainers/:id/review', to: 'trainers#review', as: 'trainer'
-  resources :cards
-  resources :decks
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:new, :create]
-  root to: 'registrations#index'
-  get 'login', to: 'sessions#new', :as => :login
-  delete 'logout', to: 'sessions#destroy'
-  post 'oauth/callback', to: 'oauths#callback'
-  get 'oauth/callback', to: 'oauths#callback' # for use with Github, Facebook
-  get 'oauth/:provider', to: 'oauths#oauth', :as => :auth_at_provider
+
+  scope module: 'home' do
+    get 'oauths/oauth'
+    get 'oauths/callback'
+    resources :sessions, only: [:new, :create]
+    resources :registrations, only: [:new, :create]
+    root to: 'registrations#index'
+    get 'login', to: 'sessions#new', :as => :login
+    post 'oauth/callback', to: 'oauths#callback'
+    get 'oauth/callback', to: 'oauths#callback' # for use with Github, Facebook
+    get 'oauth/:provider', to: 'oauths#oauth', :as => :auth_at_provider
+  end
+
+  scope module: 'dashboard' do
+    resources :sessions, only: [:destroy]
+    resource :user, only: [:edit, :update, :show]
+    post 'trainers/:id/review', to: 'trainers#review', as: 'trainer'
+    resources :cards
+    resources :decks
+    delete 'logout', to: 'sessions#destroy'
+  end
+
   scope '(:locale)', locale: /en|ru/ do
     resources :login
     resources :registrations
   end
+
 end
